76bdf6dc13d4a2d94f2dbe9f384e32012f4f2dd5
sowresunil <156076790+sowresunil@users.noreply.github.com>
2024-01-09 16:04:08 +0530

    Add files via upload

---
+++ Biggest.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Biggest 
+{
+	public static void main(String[] args) 
+	{
+		Scanner s=new Scanner("system.in");
+		int a,b;
+		System.out.println("enter two nos");
+		a=s.nextInt();
+		b=s.nextInt();
+		if(a>b)
+		{
+			system.out.println(a+"is biggest");
+		}
+		else
+		{
+			system.out.println(b+"is biggest");
+		}
+	}
+		
+	}
+}
